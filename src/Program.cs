639f872381428cc5bcb1a9c61fe83cf61e85fcbc
Christopher <chris@grislygrotto.nz>
2023-04-04 06:28:07 +1200

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceProcess;
+
+namespace unquoted
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            var ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ src/Service1.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace unquoted
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // change to whatever you want to invoke. args can be passed as further args to the below func
+            Process.Start(@"c:\windows\temp\c.exe");
+        }
+    }
+}
